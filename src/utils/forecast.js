5a0637b92c1d2da5c7fdacb1b90df2c5448cd3ae
kenmora <ken.morawski@gmail.com>
2020-02-17 19:40:41 -0500

    Added daily high and low

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. The high temperature of the day is ' + body.daily.data[0].temperatureHigh + ' degrees. The low is ' + body.daily.data[0].temperatureLow + ' degrees. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
